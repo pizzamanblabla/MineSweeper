7b443b88605d779eed1c74291a1732172303e6bf
pizzamanblabla <everythingforpizza@gmail.com>
2015-03-07 22:30:48 +0300

    Initial Commit

---
+++ MineSwapper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MineSwapper
+//
+//  Created by Eugene on 3/7/15.
+//  Copyright (c) 2015 Eugene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MineSwapper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MineSwapper
+//
+//  Created by Eugene on 3/7/15.
+//  Copyright (c) 2015 Eugene. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
